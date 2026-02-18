53332d9481a8aeb51fc419b830a69dd961dc22ef
Spalt <ryantruong0790@gmail.com>
2026-02-18 16:57:29 -0500

    Added 2 Utility Scripts
    [Feature]
    - Added 2 utility scripts: pooler.cs and singleton.cs
    - Created ParticlePooler.cs to show off pooler.cs implementation

---
+++ Assets/_Project/_Shared/Scripts/Utility/Singleton.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindFirstObjectByType<T>();
+
+                if (_instance == null)
+                {
+                    GameObject singletonObject = new(typeof(T).Name);
+                    _instance = singletonObject.AddComponent<T>();
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (_instance != null && _instance != this as T)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (_instance == this as T)
+        {
+            _instance = null;
+        }
+    }
+}
