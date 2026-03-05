28b5b81c899f8492f5e603f31d7674c176d029e1
meling20 <87735883+MemphisASC21@users.noreply.github.com>
2026-03-04 22:03:51 -0500

    Restart and quit buttons

---
+++ Assets/RestartScene.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScene : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
