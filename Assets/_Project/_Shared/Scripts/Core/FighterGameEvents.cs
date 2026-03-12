e0e51414b18aa3885512862b7e70d7a971a24db0
Spalt <ryantruong0790@gmail.com>
2026-03-12 00:24:00 -0400

    Match Logic Complete
    [Feature]
    - Timer between rounds
    - Fighters spawn between scene reload
    - Countdown occurs every round
    
    [Bug]
    - Players can move during countdown; might keep this in but remove attacking input while timer is up
    
    [Notes]
    - Fix PlayerStateMachine Input to include inputbuffer

--- Assets/_Project/_Shared/Scripts/Core/FighterGameEvents.cs
+++ Assets/_Project/_Shared/Scripts/Core/FighterGameEvents.cs
@@ -89,10 +89,11 @@ public struct TimerChangedEvent
 
 public struct CountdownUpdateEvent
 {
-    public string CountdownText;
-    public CountdownUpdateEvent(string text)
+    public float CountdownTime;
+    public int RoundCount;
+    public CountdownUpdateEvent(float countdownTime, int roundCount)
     {
-        CountdownText = text;
+        CountdownTime = countdownTime;
+        RoundCount = roundCount;
     }
 }
-
--- Assets/_Project/_Shared/Scripts/UI/TEST/RoundCountdown.cs
+++ Assets/_Project/_Shared/Scripts/UI/TEST/RoundCountdown.cs
@@ -8,24 +8,43 @@ public class RoundCountdown : MonoBehaviour
     private void Awake()
     {
         _countdownText = GetComponent<TextMeshProUGUI>();
+        _countdownText.enabled = false;
     }
 
     private void OnEnable()
     {
-        
+        FighterGameEvents.OnCountdownUpdate += UpdateCountdownDisplay;
     }
 
     private void OnDisable()
     {
-        
+        FighterGameEvents.OnCountdownUpdate -= UpdateCountdownDisplay;
     }
 
-    private void UpdateCountdownDisplay()
+    private void UpdateCountdownDisplay(CountdownUpdateEvent countdownEvent)
     {
-        //Round n
-        //Wait...
-        //Then 3, 2, 1
-        //Wait...
-        //Fight!
+        if (countdownEvent.CountdownTime == 0)
+        {
+            _countdownText.enabled = false;
+        }
+        else
+        {
+            _countdownText.enabled = true;
+        }
+
+        //If the countdownTime is negative, it means the display should show the round number instead of countdown
+        if (countdownEvent.CountdownTime < 0)
+        {
+            int totalRounds = countdownEvent.RoundCount;
+            _countdownText.text = $"Round {totalRounds}";
+        }
+        else if (countdownEvent.CountdownTime > 1)
+        {
+            _countdownText.text = (countdownEvent.CountdownTime - 1).ToString("F0");
+        }
+        else if (countdownEvent.CountdownTime == 1)
+        {
+            _countdownText.text = "FIGHT!";
+        }
     }
 }
